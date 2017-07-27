593cb17bcfd401aa5249a19f196e5d865be70d9e
afpatmin <patrick.minogue@appfoundry.se>
2017-07-27 14:18:20 +0200

    publish

---
+++ auth.php
@@ -0,0 +1,116 @@
+<?
+
+const CLIENT_NAME = "";
+const CLIENT_KEY = "";
+
+class ViAuth
+{
+    public function __construct()
+    {
+        $this->_authorization = base64_encode(CLIENT_NAME . ':' . CLIENT_KEY);
+    }
+
+    public function loginWithToken($token)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/login_with_token';
+        $fields['token'] = $token;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function loginWithUsernamePassword($username, $password)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/login_with_username_password';
+        $fields['username'] = $username;
+        $fields['password'] = $password;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function register($username, $password = null)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/register';
+        $fields['username'] = $username;
+        $fields['password'] = $password;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function resetPassword($username, $password, $token)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/reset_password';
+        $fields['username'] = $username;
+        $fields['password'] = $password;
+        $fields['token'] = $token;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function resetToken($username)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/reset_token';
+        $fields['username'] = $username;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function unregister($username)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/unregister';
+        $fields['username'] = $username;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    public function validateToken($token)
+    {
+        $curl = curl_init();
+        $url = $this->_apiBase . 'users/validate_token';
+        $fields['token'] = $token;
+        curl_setopt_array($curl, $this->_generateBody($url, $fields));
+        $response = curl_exec($curl);
+        http_response_code(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+        return $response;
+    }
+
+    private function _generateBody($url, $fields)
+    {
+        return array
+        (
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 1,
+            CURLOPT_HTTPHEADER => array('Authorization: Basic ' . $this->_authorization, 'Content-Type: application/json'),
+            CURLOPT_URL => $url,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => json_encode($fields)
+        );
+    }
+
+    private $_authorization;
+    private $_apiBase = 'https://auth.minoch.com/v1/';
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Authlib
+PHP library for use with the ViAuth API (auth.minoch.com).
+
+**Please note that the API is currently not available for public use.** 
+
+## Server-side installation
+
+#### Step 1 - Fetch files using composer:
+composer.json should look like this (ignoring any other dependencies): 
+```
+{
+    "minimum-stability": "dev",
+    "repositories":
+    [
+      {
+        "type": "vcs",
+        "url": "https://github.com/afpatmin/ViAuthServer"
+      }
+    ],
+    "require": 
+    {
+        "afpatmin/ViAuthServer": "dev-master"
+    }
+}
+```
+
+#### Step 2 - Setup file access:
+The installation contains two files of importance
+* auth.php
+* index.php
+
+Move [index.php] to a public web folder, and make sure [auth.php] is not accessible 
+from the web.
+
+Edit [index.php], at the top of the file, set AUTH_BASE to the relative path where 
+[auth.php] is located (ex. *'../../lib/auth.php'*). Set HTTP_ORIGIN to your the 
+domain from which you will call the API.
+
+#### Step 3 - Set credentials
+Edit [auth.php], at the top of the file, insert your API credentials under CLIENT_NAME 
+and CLIENT_KEY
+
+ 
